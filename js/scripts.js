7e66193e2a5a0aad571653e889678a0712599b57
Grant L McDonald <32851899+gv-webguru@users.noreply.github.com>
2019-01-25 16:08:43 -0500

    End of day commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+/*Scripts for Loth RSVP Campaign.*/
+$(document).ready(function(){
+    console.log("Ready to rock!");
+    
+    $("#yes").click(function(ev){
+        ev.preventDefault();
+        console.log("I'm going.");
+        $("#kickoffRSVP").val("yes");
+        $("#rsvp-form").submit();
+    });
+    
+    $("#no").click(function(ev){
+        ev.preventDefault();
+        console.log("I have better things to do.");
+        $("#kickoffRSVP").val("no");
+        $("#rsvp-form").submit();
+    });
+    
+    var going = document.getElementById("going").value;
+    var msg;
+    if(going === "yes"){
+       msg = "Excellent! We'll see you there.";
+        $(".message p").html(msg);
+       }
+    if(going === "no"){
+        msg = "We're sorry you won't be able to make it to the the 2019 Kick-Off. We hope you see you at our next big event.";
+        $(".message p").html(msg);
+    }
+    
+    console.log(msg);
+});
